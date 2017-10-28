7c5bd3bac4ee91a4292398068730791b9d013311
Diego Correa <diegocorrea.cc@gmail.com>
2017-10-28 00:34:12 -0300

    adicionando leitura da LUT

--- marching_cubes/marching.c
+++ marching_cubes/marching.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-# define DEBUG 1
+#define DEBUG 1
+#define LUTLINES 256
+#define LUTCOLUMN 16
+
 FILE *fl_DEBUG;
+int LUT[LUTLINES][LUTCOLUMN];
 
 void readFile(int argc, char *argv[]);
 
@@ -35,6 +39,7 @@ int main(int argc, char *argv[]) {
 }
 
 void readFile(int argc, char *argv[]){
+	FILE *fl_input;
 	// <DEBUG>
 	if (DEBUG == 1){
 		printf("[readFile] - Iniciando Função\n");
@@ -42,7 +47,31 @@ void readFile(int argc, char *argv[]){
 		fprintf(fl_DEBUG, "[readFile] - Lendo do arquivo: %s\n", argv[1]);
 	} // </DEBUG>
 
+	fl_input = fopen(argv[1], "r" );
+	if (fl_input == 0) {
+        printf("[readFile] - Falar ao tentar abrir: %s\n", argv[1]);
+        exit(1);
+    }
 
+    for(int i = 0; i < LUTLINES; i++) {
+    	for(int j = 0; j < LUTCOLUMN; j++){
+    	   	fscanf(fl_input, "%d", &LUT[i][j]);
+    	}
+    }
+    // <DEBUG>
+	if (DEBUG == 1){
+		for(int i = 0; i < LUTLINES; i++) {
+			fprintf(fl_DEBUG, "[readFile] Linha '%d': - ", i+1);
+	    	for(int j = 0; j < LUTCOLUMN; j++){
+				fprintf(fl_DEBUG, "%d ", LUT[i][j]);
+	    	}
+	    	fprintf(fl_DEBUG, "\n");
+	    }
+	} // </DEBUG>
+
+	fscanf(fl_input, "%d", &planSize);
+
+	fclose( fl_input );
 	// <DEBUG>
 	if (DEBUG == 1){
 		printf("[readFile] - Finalizando Função\n");
