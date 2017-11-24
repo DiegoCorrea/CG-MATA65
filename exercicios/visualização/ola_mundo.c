cf1fdab1501c7e7114bf8467b0c2208c7d46d477
Diego Correa <diegocorrea.cc@gmail.com>
2017-11-23 21:35:56 -0300

    adicionando trabalho 2 e exercicio de sala

---
+++ exercicios/visualização/ola_mundo.c
@@ -0,0 +1,68 @@
+// OpenGL Utility Toolkit
+#include <GL/glut.h>
+
+// Apenas requerido pelas aplicacoes em windows
+//#include <windows.h>
+
+// Inicializacoes de OpenGL que devem ser
+// executadas antes da exibicao do desenho
+void Inicializa(){
+
+  // Define a janela de visualizacao
+  glMatrixMode(GL_PROJECTION);
+
+  // Define o sistema de coordenadas
+  glFrustum(-1.0, 1.0, -1.0, 1.0, 1.0, 3.0);
+
+  gluLookAt(0.0, 0.0, 5.0, 0.5, 1.0, 0.0, 1.0, 1.0, 0.0);
+
+  // Define a cor de fundo da janela como azul
+  glClearColor(0.0, 0.0, 1.0, 1.0);
+}
+
+// Funcao callback chamada para fazer o desenho
+void Desenha(){
+
+  // Limpa a janela de visualizacao com a cor
+  // de fundo especificada
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // Define a cor de desenho como vermelho
+  glColor3f(1.0, 0.56, 0.0);
+
+  // Desenha um triangulo
+  glBegin(GL_QUADS);
+    glVertex3f(-1.0, -1.0, 3.0);
+    glVertex3f(1.0, -1.0, 3.0);
+    glVertex3f(1.0, 1.0, 3.0);
+    glVertex3f(-1.0, 1.0, 3.0);
+  glEnd();
+
+  // Executa os comandos OpenGL para renderizacao
+  glFlush();
+}
+
+// Programa principal
+int main(int argc, char** argv){
+
+  // Inicia GLUT e processa argumentos passados por linha de comandos
+  glutInit(&argc, argv);
+
+  // Avisa a GLUT que tipo de modo de exibicao deve ser usado quando a janela eh criada
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+  // Cria uma janela GLUT que permite a execucao de comandos OpenGL
+  glutCreateWindow("Ola Mundo!");
+
+  // Define a funcao responsavel por redesenhar a janela OpenGL sempre que necessario
+  glutDisplayFunc(Desenha);
+
+  // Inicializacoes de OpenGL executadas antes da exibicao do desenho
+  Inicializa();
+
+  // Inicia o processamento de eventos de GLUT. O controle do programa
+  // passa a GLUT, que inicia o gerenciamento dos eventos
+  glutMainLoop();
+
+  return 0;
+}
