c58cb8797c7a7a510096bc0b86e4809526d4aaad
Diego Correa <diegocorrea.cc@gmail.com>
2017-12-08 21:09:59 -0300

    iniciando a passar o filtro

--- imageRestorationOpenGL/imageRestoration.cpp
+++ imageRestorationOpenGL/imageRestoration.cpp
@@ -22,23 +22,50 @@ unsigned char* data;        // Actual RGB data
 unsigned char* data_histogram; // Actual RGB data
 int grayLevel[256] = {0};
 
-void gaussianFunction(int distance, int factor){
+const int phiS = 3;
+const int phiR = 8;
+const int pixelWindow = 9*9;
+
+float gaussianFunction(int distance, int factor) {
 	float eulerNumber = exp(-(pow(distance/(2*factor), 2.0)));
 	//float eulerNumber = exp(-(pow(distance, 2.0)/(2*pow(factor, 2.0))));
 	float firstPart = 1/(factor*sqrt(2*PI));
 	//float firstPart = 1/(2*(3.14*(factor^2)));
+	float result = firstPart*eulerNumber;
 
 	// <LOGGER>
 	if (LOGGER){
 		fprintf(fl_LOGGER, "[gaussianFunction] - Iniciando\n");
 		fprintf(fl_LOGGER, "[gaussianFunction] - O Valor do numero de Euler: %f\n", eulerNumber);
 		fprintf(fl_LOGGER, "[gaussianFunction] - Valor da primeira parte: %f\n", firstPart);
-		fprintf(fl_LOGGER, "[gaussianFunction] - Resultado da função Gaugasiana: %f\n", firstPart*eulerNumber);
+		fprintf(fl_LOGGER, "[gaussianFunction] - Resultado da função Gaugasiana: %f\n", result);
 		fprintf(fl_LOGGER, "[gaussianFunction] - Finalizando\n");
 	} // </LOGGER>
+	return result;
 }
 
-int loadImageBMP(const char *imagepath){
+float normalizationFactor(int pixelsPosition) {
+	float distanceBetweenCoordinates;
+	float distanceBetweenColors;
+	float result;
+	for(int i = 0; i < pixelWindow; i++) {
+		result += gaussianFunction(abs(pixelsPosition - i), phiS) * gaussianFunction((data[pixelsPosition] - data[i]), phiR);
+	}
+
+	return result;
+}
+
+void bilateralFilter(){
+	data_filter = (unsigned char *)malloc(imageSize);
+
+	int i,j;
+
+	for(i = 0; i < width * height ; i++){
+		int index = i*3;
+	}
+}
+
+int loadImageBMP(const char *imagepath) {
 	// Open the file
 	FILE * file = fopen(imagepath,"rb");
 	if (!file){
@@ -46,12 +73,12 @@ int loadImageBMP(const char *imagepath){
 		return 0;
 	}
 
-	if ( fread(header, 1, 54, file)!=54 ){ // If not 54 bytes read : problem
+	if ( fread(header, 1, 54, file)!=54 ) { // If not 54 bytes read : problem
 		printf("Not a correct BMP file\n");
 		return 0;
 	}
 
-	if ( header[0]!='B' || header[1]!='M' ){
+	if ( header[0]!='B' || header[1]!='M' ) {
 		printf("Not a correct BMP file\n");
 		return 0;
 	}
@@ -85,7 +112,7 @@ int loadImageBMP(const char *imagepath){
 	fread(data,1,imageSize,file);
 
 	int i;
-	for(i = 0; i < width * height ; ++i){
+	for(i = 0; i < width * height ; ++i) {
 		int index = i*3;
 		unsigned char R, G, B;
 		B = data[index];
@@ -98,12 +125,12 @@ int loadImageBMP(const char *imagepath){
 	fclose(file);
 }
 
-void init(void){
+void init(void) {
   //glPixelStorei (GL_UNPACK_ALIGNMENT, 1);
   glClearColor (0.0, 0.0, 0.0, 0.0);
 }
 
-void display(void){
+void display(void) {
   glClear(GL_COLOR_BUFFER_BIT);
   //glColor3f (1.0, 1.0, 1.0);
 
@@ -117,7 +144,7 @@ void display(void){
   glFlush();
 }
 
-void reshape(int w, int h){
+void reshape(int w, int h) {
   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -147,7 +174,7 @@ char *getImageName(char **argv) {
 	return fileName;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 	if(argc < 2){
 		printf("Uma imagem é necessaria para aplicar o Filtro e a Máscara\n");
 		return 1;
@@ -160,7 +187,15 @@ int main(int argc, char **argv){
 	char *fileName = getImageName(argv);
 
 	loadImageBMP(argv[1]);
-	gaussianFunction(10,3);
+
+
+
+	// <LOGGER>
+	if (LOGGER){
+		fprintf(fl_LOGGER, "[main] - Finalizando processamento e exibindo tela\n");
+		fclose( fl_LOGGER );
+	} // </LOGGER>
+	return 0;
 
 
 	glutInit(&argc, argv);
@@ -173,12 +208,4 @@ int main(int argc, char **argv){
 	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(display);
 	glutMainLoop();
-
-
-
-	// <LOGGER>
-	if (LOGGER){
-		fclose( fl_LOGGER );
-	} // </LOGGER>
-	return 0;
 }
