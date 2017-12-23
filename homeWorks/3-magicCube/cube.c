c4f5aca1a06d8f5ac318fb7ad23ad3e143e6b234
Diego Correa <diegocorrea.cc@gmail.com>
2017-12-23 00:20:31 -0300

    criando parte de tras como um cubao

--- homeWorks/3-magicCube/cube.c
+++ homeWorks/3-magicCube/cube.c
@@ -22,7 +22,7 @@ void Inicializa() {
 }
 // Funcao callback chamada para fazer o desenho
 void Desenha() {
-  static GLint vertices[18*3] = { 
+  static GLint vertices[9*2*3] = { 
     -1, -1, -1, // v 0
     0, -1, -1,  // v 1
     1, -1, -1,  // v 2
@@ -43,50 +43,55 @@ void Desenha() {
     1, 1, 0     // v 17
   };
   static GLubyte faces[6*4*4] = { 
-    0, 3, 4, 1,     //Cubo 1
-    9, 10, 13, 12,
-    0, 9, 12, 3,
-    13, 10, 1, 4,
-    3, 12, 13, 4,
-    0, 1, 10, 9,    //Cubo 1
-    1, 4, 5, 2,     //cubo 2
-    10, 11, 14, 13,
-    1, 10, 13, 4,
-    11, 2, 5, 14,
-    4, 13, 14, 5,
-    1, 2, 11, 10,   //cubo 2
-    3, 6, 7, 4,     // cubo 3
-    12, 13, 16, 15,
-    3, 12, 15, 6,
-    16, 13, 4, 7,
-    6, 15, 16, 7,
-    3, 4, 13, 12,   // cubo 3
-    7, 8, 5, 4,     // cubo 4
-    16, 13, 14, 17,
-    7, 4, 13, 16,
-    17, 14, 5, 8,
-    13, 4, 5, 14,
-    16, 17, 8, 7    // cubo 4
+    // Cube 1
+    0, 3, 4, 1,     // Face 1 Color Red
+    9, 10, 13, 12,  // Face 2 Color Black
+    0, 9, 12, 3,    // Face 3 Color Blue
+    13, 10, 1, 4,   // Face 4 Color Black
+    3, 12, 13, 4,   // Face 5 Color Black
+    0, 1, 10, 9,    // Face 6 Color White
+    // Cube 2
+    1, 4, 5, 2,     // Face 1 Color Red
+    10, 11, 14, 13, // Face 2 Color Black
+    1, 10, 13, 4,   // Face 3 Color Black
+    11, 2, 5, 14,   // Face 4 Color Green
+    4, 13, 14, 5,   // Face 5 Color Black
+    1, 2, 11, 10,   // Face 6 Color White
+    // Cube 3
+    3, 6, 7, 4,     // Face 1 Color Red
+    12, 13, 16, 15, // Face 2 Color Black
+    3, 12, 15, 6,   // Face 3 Color Blue
+    16, 13, 4, 7,   // Face 4 Color Black
+    6, 15, 16, 7,   // Face 5 Color Yellow
+    3, 4, 13, 12,   // Face 6 Color Black
+    // Cube 4
+    7, 8, 5, 4,     // Face 1 Color Red
+    16, 13, 14, 17, // Face 2 Color Black
+    7, 4, 13, 16,   // Face 3 Color Black
+    17, 14, 5, 8,   // Face 4 Color Green
+    16, 17, 8, 7,   // Face 5 Color Black
+    13, 4, 5, 14    // Face 6 Color Yellow
+    //
   };
   static GLfloat cores[9*2*3] = { 
-    1.0, 1.0, 1.0, 
-    0.5, 0.5, 0.5, 
-    0.25, 0.25, 0.25, 
-    0.0, 0.0, 0.0,
-    1.0, 1.0, 1.0, 
-    0.5, 0.5, 0.5, 
-    0.25, 0.25, 0.25, 
-    0.0, 0.0, 0.0,
-    1.0, 1.0, 1.0, 
-    0.5, 0.5, 0.5, 
-    0.25, 0.25, 0.25, 
-    0.0, 0.0, 0.0,
-    1.0, 1.0, 1.0, 
-    0.5, 0.5, 0.5, 
-    0.25, 0.25, 0.25, 
-    0.0, 0.0, 0.0,
-    0.5, 0.5, 0.5, 
-    0.25, 0.25, 0.25
+    1.0, 0.0, 0.0,      // v 0 Color Red
+    1.0, 0.0, 0.0,      // v 1 Color Red
+    1.0, 0.0, 0.0,      // v 2 Color Red
+    1.0, 0.0, 0.0,      // v 3 Color Red
+    1.0, 0.0, 0.0,      // v 4 Color Red
+    1.0, 0.0, 0.0,      // v 5 Color Red
+    1.0, 0.0, 0.0,      // v 6 Color Red
+    1.0, 0.0, 0.0,      // v 7 Color Red
+    1.0, 0.0, 0.0,      // v 8 Color Red
+    0.11, 0.49, 0.95,   // v 9 Color Blue
+    1.0, 1.0, 1.0,      // v 10 Color White
+    0.2, 0.44, 0.28,    // v 11 Color Green
+    0.11, 0.49, 0.95,   // v 12 Color Blue
+    0.0, 0.0, 0.0,      // v 13 Color Black
+    0.2, 0.44, 0.28,    // v 14 Color Green
+    0.11, 0.49, 0.95,   // v 15 Color Blue
+    0.93, 0.93, 0.0,    // v 16 Color Yellow
+    0.2, 0.44, 0.28     // v 17 Color Green
   };
 
   glEnable(GL_DEPTH_TEST);
@@ -96,7 +101,7 @@ void Desenha() {
   glRotatef(angulo_x, 1, 0, 0);
   glRotatef(angulo_y, 0, 1, 0);
 
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glEnableClientState(GL_COLOR_ARRAY);
@@ -114,11 +119,19 @@ void Desenha() {
 
 void keyboard(unsigned char key, int x, int y){
   switch(key) {
-    case 'x': 
+    case 'w': 
       angulo_x++;
       glutPostRedisplay();
     break;
-    case 'y': 
+    case 's': 
+      angulo_x++;
+      glutPostRedisplay();
+    break;
+    case 'a': 
+      angulo_y++;
+      glutPostRedisplay();
+    break;
+    case 'd': 
       angulo_y++;
       glutPostRedisplay();
     break;
